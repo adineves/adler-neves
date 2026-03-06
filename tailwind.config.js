62776a276964cbedcf42b9c2980904a769a38cd0
Adler Neves <100231184+adineves@users.noreply.github.com>
2026-03-06 11:52:22 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0d0d0d",
+        secondary: "#151515",
+        accent: "#22c55e",
+      },
+    },
+  },
+  plugins: [],
+}
